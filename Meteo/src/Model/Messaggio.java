5e233986c61960b9b4c5b53e70a06eae848696a4
Michele Bortot <miky97it@gmail.com>
2017-11-07 12:28:03 +0100

    sd

--- Meteo/src/Model/Messaggio.java
+++ Meteo/src/Model/Messaggio.java
@@ -3,7 +3,7 @@ package Model;
 public class Messaggio {
 	/**
 	 * É una citta' o provincia*/
-	private String Localita;
+	private String localita;
 	/**
 	 *1 soleggiato
 	 *2 nubi sparse
@@ -12,11 +12,42 @@ public class Messaggio {
 	 *5 temporale con fulmini
 	 *6 neve
 	 **/
-	private int Tempo;
-	
+	private int tempo;
+	private float temperatura;
+	private int vento;
+	public Messaggio(){}
+	public String getLocalita() {
+		return localita;
+	}
+	public void setLocalita(String localita) {
+		this.localita = localita;
+	}
+	public int getTempo() {
+		return tempo;
+	}
+	public void setTempo(int tempo) {
+		this.tempo = tempo;
+	}
+	public float getTemperatura() {
+		return temperatura;
+	}
+	public void setTemperatura(float temperatura) {
+		this.temperatura = temperatura;
+	}
+	public int getVento() {
+		return vento;
+	}
+	public void setVento(int vento) {
+		this.vento = vento;
+	}
+	public int getCmpioggia() {
+		return cmpioggia;
+	}
+	public void setCmpioggia(int cmpioggia) {
+		this.cmpioggia = cmpioggia;
+	}
+	private int cmpioggia;
 	public static void main(String[] args) {
-	
-
 	}
 
 }
