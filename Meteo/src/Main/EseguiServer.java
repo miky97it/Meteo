dcb60da0f655cfef457731aef97d9dd75d786ff9
Michele Bortot <miky97it@gmail.com>
2017-11-08 12:46:26 +0100

    documentazione

---
+++ Meteo/src/Main/EseguiServer.java
@@ -0,0 +1,16 @@
+package Main;
+
+import Server.Server;
+
+public class EseguiServer {
+
+	public static void main(String[] args) {
+		try {
+			Server S=new Server(1234);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
